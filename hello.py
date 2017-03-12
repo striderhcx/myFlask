31079d95e4f47b4820d93cd5fbbef768daa8982f
striderhcx <1225427292@qq.com>
2017-03-12 16:56:17 +0800

    Chapter 2: Command line options with Flask-Script (2c)

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
 
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -12,5 +15,5 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 
