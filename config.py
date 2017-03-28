a1696f053c567a349f47b7194d44d6fa6c043ec9
striderhcx <1225427292@qq.com>
2017-03-28 21:13:58 +0800

    Chapter 11: Blog post pagination (11d)

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = '1225427292@qq.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
